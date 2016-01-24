09609f8477879b4a5d200f36582bc6050a7c7053
xingxing.wang <xingxing.wang@tqmall.com>
2016-01-24 17:44:16 +0800

    nlp 模块添加

---
+++ commons-nlp/src/main/java/com/tqmall/search/commons/nlp/Pinyin.java
@@ -0,0 +1,12 @@
+package com.tqmall.search.commons.nlp;
+
+/**
+ * Created by xing on 16/1/24.
+ * 拼音操作类
+ */
+public final class Pinyin {
+
+    private Pinyin(){
+    }
+
+}
---
+++ commons-nlp/src/main/java/com/tqmall/search/commons/utils/PinyinUtils.java
@@ -0,0 +1,12 @@
+package com.tqmall.search.commons.utils;
+
+/**
+ * Created by xing on 16/1/24.
+ * 拼音操作类
+ */
+public final class PinyinUtils {
+
+    private PinyinUtils(){
+    }
+
+}
